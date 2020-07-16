ea03e08b1beb193c2d77a9aed0a117ebac681b09
PotatoMani <52829594+PotatoMani@users.noreply.github.com>
2020-07-16 14:55:39 +0800

    finish something

--- data/php/function/MysqlControl.php
+++ data/php/function/MysqlControl.php
@@ -3,18 +3,22 @@
 <?php
 //本函数用来连接数据库
 function connectMysql($host/*数据库url*/, $user/*用户名*/, $password/*密码*/,
-                      $database/*数据库名*/, $charset/*编码方式*/){
+                      $database/*数据库名*/, $charset/*字符集*/){
     $dsn = sprintf("mysql:host = %s; dbname = %s; charset = %s",
         $host, $database, $charset
     );
 
     try {
         $pdo = new PDO($dsn, $user, $password);// 连接
-        $pdo->setAttribute(PDO::ATTR_ERRMODE,
+        $pdo -> setAttribute(PDO::ATTR_ERRMODE,
             PDO::ERRMODE_EXCEPTION);//错误处理方式设置
-
+        $pdo -> exec("USE %s",$database);
     } catch (PDOException $e) { // 错误处理
         die($e->getMessage());
     }
     return $pdo;
+}
+//本函数用于创建表
+function createTable($pdo/*继承PDO*/, $tableName/*表名*/, $tableCharset/*表字符集*/){
+
 }
