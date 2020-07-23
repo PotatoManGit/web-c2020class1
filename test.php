4a51928997bbfad97ea13591eb8e7f5973ed5c4b
potatoman <potatoman189@163.com>
2020-07-23 22:49:52 +0800

    something is ok

--- test.php
+++ test.php
@@ -7,42 +7,28 @@
     <link rel="stylesheet" href="/wangEditor-master/release/mainStyle.css">
 </head>
 <body class="indexBodySetting">
-<div id="editor" style="background: #ffffff">
-    <p>输入些什么吧</p>
-</div><br/>
-<form action="test.php">
-    <center><button class="buttonStyle_01" id="check" type="submit">提交</button></center>
-</form>
+<div id="div1">
+    <p>欢迎使用 wangEditor 编辑器</p>
+</div>
+<button id="btn1">获取html</button>
+<button id="btn2">获取text</button>
 
-
-<script type="text/javascript" src="/wangEditor-master/release/wangEditor.min.js"></script>
+<script type="text/javascript" src="wangEditor-master/release/wangEditor.min.js"></script>
 <script type="text/javascript">
-    import $ from "./wangEditor-master/src/js/util/dom-core";
-
     var E = window.wangEditor
-    var editor = new E('#editor')
-    // editor.customConfig.uploadImgShowBase64 = true //使用Base64
-    // editor.customConfig.uploadImgServer = 'http://c2020class1.potatost.xyz/'
+    var editor = new E('#div1')
     editor.create()
 
-    document.getElementById('check').addEventListener('click', function () {
+    document.getElementById('btn1').addEventListener('click', function () {
         // 读取 html
-        var XHR = new XMLHttpRequest();
-        var FD  = new FormData();
+        alert(editor.txt.html())
+    }, false)
 
-        // var writeData = document.getElenmentById("username").editor.txt.html();
-        // // alert(writeData);
-        // winodw.open('test.php?username=' + writeData)
-
+    document.getElementById('btn2').addEventListener('click', function () {
+        // 读取 text
+        alert(editor.txt.text())
     }, false)
 
 </script>
 </body>
 </html>
-<?php
-
-$keyword="<script>document.writeln(writeData);</script>";//php获取js的变量！！
-
-echo "$keyword";
-
-?>
